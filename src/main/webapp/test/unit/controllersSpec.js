f3b12afea90c3848d3850f45758beffa740b19f2
Octavian Popa <octavian.popa@windeln.ro>
2016-02-18 01:05:40 +0200

    Added dummy js unit test

--- src/main/webapp/test/unit/controllersSpec.js
+++ src/main/webapp/test/unit/controllersSpec.js
@@ -3,5 +3,8 @@
 /* jasmine specs for controllers go here */
 describe('controllers', function () {
 
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
 
 });
