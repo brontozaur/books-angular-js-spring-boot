16e0524cc54d3dc22f30328769ef407ac61de5e0
Octavian Popa <octavian.popa@windeln.ro>
2016-02-18 02:05:06 +0200

    Added first real js unit test! yay!

--- src/main/webapp/test/karma.conf.js
+++ src/main/webapp/test/karma.conf.js
@@ -6,8 +6,11 @@ module.exports = function(config){
     files : [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/bower_components/angular-animate/angular-animate.js',
+      'app/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'app/bower_components/jquery/dist/jquery.js',
+      'app/bower_components/bootstrap/dist/js/bootstrap.js',
+      'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+       'app/bower_components/angular-ui-grid/ui-grid.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/js/**/*.js',
       'test/unit/**/*.js'
--- src/main/webapp/test/unit/servicesSpec.js
+++ src/main/webapp/test/unit/servicesSpec.js
@@ -2,4 +2,12 @@
 
 describe('service', function () {
 
+    // load modules
+    beforeEach(module('BooksManager'));
+
+    // Test service availability
+    it('check the existence of EdituriService', inject(function(EdituriService) {
+        expect(EdituriService).toBeDefined();
+    }));
+
 });
